c628c3360b3192b57f72d0c00bed273255e88e20
Osiel Hernández <osielhernandez@MacBook-Pro-de-Osiel.local>
2017-04-11 01:55:16 -0500

    Footer responsivo

--- development/js/scripts.js
+++ development/js/scripts.js
@@ -33,11 +33,36 @@
 
 	//Publicidad
 	$('.mag-advertising-slider').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
+		
 		dots: false,
 		centerMode: true,
-		focusOnSelect: true
+		focusOnSelect: true,
+		responsive: [
+		    {
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+		    },
+		    {
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+		    },
+		    {
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		]
 	});
 
 
